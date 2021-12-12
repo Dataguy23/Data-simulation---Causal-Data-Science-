3389b9af5f53435705402ab5b11a7140d06530da
Dataguy23 <83167407+Dataguy23@users.noreply.github.com>
2021-12-12 14:50:24 +0100

    bug fixes

--- dataSimulation.py
+++ dataSimulation.py
@@ -15,10 +15,10 @@ IndustryExperience = [] # bool
 FlexibleWork = [] # Cont
 ExtrinsicReward = [] # Cont
 EducationLevel = [] # int 
-OrganizationalCommitment = [] # int
+Internship = [] # 
 AdvancementOpportunities = [] # int
 JobSatisfaction = [] # int
-
+PreConception = [] # 
 
 
 # Generating data for each variable, except Retention
@@ -27,20 +27,26 @@ for i in range(n):
     
     (IndustryExperience.append(0) if Age[i]<26 else IndustryExperience.append(random.randint(0,1))) # setting industry experience to an arbitrary function of age, at least 26 years of age to be eligble for industry experience
     
+    (Internship.append(0) if Age[i]>40 else Internship.append(random.randint(0,1))) # considering retention is not more than 10 years, it seems fair  to set internship==0 if Age >40 (did not start out as intern at +30), else random
+    
     (FirstJob.append(0) if IndustryExperience[i]==1 else FirstJob.append(random.randint(0,1))) # setting firstjob to FALSE if industry experience is TRUE, else TEMP random 
     FirstJob[i] = 0 if Age[i] >33 else FirstJob[i] # setting FirstJob to FALSE if age is above 33, else keep random integer in set {0,1}
     
     (FlexibleWork.append(random.randint(45,70)) if FirstJob[i]==1 else FlexibleWork.append(random.randint(60,90)*(100-Age[i])/100)) # setting flexible work as a func of first job and age, in which newly and younger enployees are more likely to work long hours    
     
     ExtrinsicReward.append(round((base_salary+bonus_salary)*(FlexibleWork[i]*0.04)*(Age[i]/100)*(1+(IndustryExperience[i]*0.1)),ndigits=2)) # setting extrinsic as an arb func of age and industry expertise
-
-    (OrganizationalCommitment.append(random.randint(2,8)) if ExtrinsicReward[i]>65000 else OrganizationalCommitment.append(random.randint(1,4))) # setting the org commitment as a func of extrinsicReward
  
-    (EducationLevel.append(random.randint(2,4)) if FirstJob[i]==1 else EducationLevel.append(random.randint(1,4))) # setting education to a func first job, specificially education is at least bachelor if this is the first job, else random 
+    (EducationLevel.append(random.randint(2,4)) if FirstJob[i]*Age[i]*random.random()>25 else EducationLevel.append(random.randint(1,4))) # setting education to a func of first job * age * random
 
     AdvancementOpportunities.append(round(np.sum((IndustryExperience[i]*5+EducationLevel[i])))) # setting advancements to equal the sum of industry expertise and education level
 
-    JobSatisfaction.append(AdvancementOpportunities[i]+OrganizationalCommitment[i]+ExtrinsicReward[i]*0.001+FlexibleWork[i]*-0.3) # setting jobSatisfaction equal to causes, regulating the impact of reward and flexibleWork
+    JobSatisfaction.append(AdvancementOpportunities[i]+ExtrinsicReward[i]*0.001+FlexibleWork[i]*-0.3) # setting jobSatisfaction equal to causes, regulating the impact of reward and flexibleWork
+
+    PreConception.append(Age[i]*random.random()) # function of AGE
+    PreConception[i] = 1 if PreConception[i]>30 else 0 # function of AGE
+    PreConception[i] = 1 if IndustryExperience[i]==1 else PreConception[i] # setting PreConception 1 (TRUE) in case of IndustryExperience
+    PreConception[i] = 1 if Internship[i] ==1 else PreConception[i] # setting PreConception 1 (TRUE) in case of Internship
+
 
 
 # Scale jobSatisfaction with linear transformation to be in range of MaxVal - MinVal
@@ -55,7 +61,7 @@ for i in range(n):
 
 # Generating data for Retention
 for i in range(n):
-    Retention.append(FirstJob[i]*2+JobSatisfaction[i]+IndustryExperience[i]*2) # Setting retention to a func of the cuases, assigning more value to binary values for them to have some influence
+    Retention.append(PreConception[i]*2+JobSatisfaction[i]) # Setting retention to a func of the cuases, assigning more value to binary values for them to have some influence
 
 # Scale Retention with linear transformation to be in range of MaxVal - MinVal
 MaxVal = 10
@@ -68,9 +74,8 @@ for i in range(n):
 
 
 # Converting data to a pd DataFrame
-df = pd.DataFrame(list(zip(Retention,FirstJob,Age,IndustryExperience,FlexibleWork,ExtrinsicReward,EducationLevel,OrganizationalCommitment,AdvancementOpportunities,JobSatisfaction)),
-columns=["Retention","FirstJob","Age","IndustryExperience","FlexibleWork","ExtrinsicReward","EducationLevel","OrganizationalCommitment","AdvancementOpportunities","JobSatisfaction"])
+df = pd.DataFrame(list(zip(Retention,FirstJob,Age,IndustryExperience,FlexibleWork,ExtrinsicReward,EducationLevel,PreConception,Internship,AdvancementOpportunities,JobSatisfaction)),
+columns=["Retention","FirstJob","Age","IndustryExperience","FlexibleWork","ExtrinsicReward","EducationLevel","PreConception","Internship","AdvancementOpportunities","JobSatisfaction"])
 
 
-df.to_csv("../SimulatedExamData.csv",index=0)
-
+df.to_csv("./SimulatedExamData.csv",index=0)
